74c7bbee8bb1716ca5823b281a599ea5724a7a2c
cpccharry <harry.subramaniam@cpcc.edu>
2021-10-09 10:21:08 -0400

    Initial release.

---
+++ src/test/java/edu/cpcc/labs/SpringbootGtWozuLabthreeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGtWozuLabthreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
